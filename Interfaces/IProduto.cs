e12270445f6cc119536abdccf7e563f3b1d5d546
ArkDreal <andrey.o.leal@gmail.com>
2021-06-09 12:45:01 -0300

    Produto concluido

--- Interfaces/IProduto.cs
+++ Interfaces/IProduto.cs
@@ -1,7 +1,12 @@
+using System.Collections.Generic;
+using ProjetoProdutos.Classes;
+
 namespace ProjetoProdutos.Interfaces
 {
-    public class IProduto
+    public interface IProduto
     {
-        
+        string Cadastrar (Produto produto);
+        List<Produto> Listar();
+        string Deletar(Produto produto);
     }
 }
