d7aa552f5f56a9ee7a267e3b80771e9c46ea4bfc
ArkDreal <andrey.o.leal@gmail.com>
2021-06-09 12:35:37 -0300

    Marca concluida

--- Classes/Marca.cs
+++ Classes/Marca.cs
@@ -1,7 +1,47 @@
+using System;
+using System.Collections.Generic;
+using ProjetoProdutos.Interfaces;
+
+
 namespace ProjetoProdutos.Classes
 {
-    public class Marca
+     public class Marca : IMarca
     {
+        // Atributos
+        private int Codigo { get; set; }
+        private string NomeMarca { get; set; }
+        private DateTime DataCadastro { get; set; }
+
+        // Construtores
+        public Marca() {
+
+        }
+
+        public Marca(int _codigo, string _nomeMarca) {
+            this.Codigo = _codigo;
+            this.NomeMarca = _nomeMarca;
+            this.DataCadastro = DateTime.Now;
+        }
         
+        // Lista de Marcas
+        List<Marca> listaMarcas = new List<Marca>();
+
+        // Métodos
+        public string Cadastrar(Marca marca)
+        {
+            listaMarcas.Add(marca);
+            return ($"Marca {marca.NomeMarca} cadastrada");
+        }
+
+        public string Deletar(Marca marca)
+        {
+            listaMarcas.Remove(marca);
+            return ($"Marca {marca.NomeMarca} deletada");
+        }
+
+        public List<Marca> Listar()
+        {
+            return listaMarcas;
+        }
     }
 }
--- Interfaces/IMarca.cs
+++ Interfaces/IMarca.cs
@@ -1,7 +1,14 @@
+using System.Collections.Generic;
+using ProjetoProdutos.Classes;
+
 namespace ProjetoProdutos.Interfaces
 {
-    public class IMarca
+    public interface IMarca
     {
-        
+        string Cadastrar(Marca marca);
+
+        List<Marca> Listar();
+
+        string Deletar(Marca marca);
     }
 }
